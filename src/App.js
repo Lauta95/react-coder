2e0c26642d4d43db6a600cf21099c6b9322b721f
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-04-01 23:08:39 -0300

    se agrega el greeting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HomeView } from './components/HomeView/HomeView';
+import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
@@ -6,8 +6,7 @@ function App() {
     <>
       <div>
         <NavBar />
-        <h1>holaa mundo</h1>
-        <HomeView />
+        <ItemListContainer greeting='Hola Mundo' />
       </div>
     </>
   );
