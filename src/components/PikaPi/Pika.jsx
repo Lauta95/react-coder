dfa840321e15323d5a466a4160b72b2e8270a9fc
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-04-22 21:43:51 -0300

    buscador de pokemon

--- src/components/PikaPi/Pika.jsx
+++ src/components/PikaPi/Pika.jsx
@@ -12,41 +12,57 @@ export const Pika = () => {
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
             .then((res) => res.json())
-            .then((data) =>{
+            .then((data) => {
                 setPokemon({
                     numero: data.id,
                     nombre: data.name,
-                    img: data.sprites.front_default
+                    img: data.sprites.front_default,
                 })
             })
     }, [id])
 
     const anterior = () => {
-        id > 1 && setId (id - 1)
+        id > 1 && setId(id - 1)
     }
 
     const siguiente = () => {
         setId(id + 1)
     }
 
-    const busqueda = (e) => {
+    const inputChange = (e) => {
         setBusqueda(e.target.value)
+    }
+    const submit = (e) => {
+        e.preventDefault()
+        if (busqueda.length > 2) {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${busqueda}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setPokemon({
+                        nombre: data.name,
+                        img: data.sprites.front_default,
+                    })
+                    setId(
+                        data.id
+                    )
+                })
+        }
     }
 
     return (
         <>
-        <h3 className="pika">{pokemon.numero}</h3>
-        <h3 className="pika">{pokemon.nombre}</h3>
-        <div className="pika">
-        <img src={pokemon.img}/>
-        </div>
-        <div className="pika">
-            <button onClick={anterior}>Anterior</button>
-            <button onClick={siguiente}>Siguiente</button>
-        </div>
-        <div className="pika">
-            <input type="text"  value={busqueda}/>
-        </div>
+            <h3 className="pika">{pokemon.numero}</h3>
+            <h3 className="pika">{pokemon.nombre}</h3>
+            <div className="pika">
+                <img src={pokemon.img} />
+            </div>
+            <div className="pika">
+                <button onClick={anterior}>Anterior</button>
+                <button onClick={siguiente}>Siguiente</button>
+            </div>
+            <form onSubmit={submit} className="pika">
+                <input type="text" value={busqueda} onChange={inputChange} />
+            </form>
         </>
     )
 }
