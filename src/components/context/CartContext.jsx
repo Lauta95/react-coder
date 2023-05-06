448e5a2ad97dad97cbcd08c5eb32f3685467c8ed
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-06 08:43:11 -0300

    se agrega local storage

--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -1,11 +1,18 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from 'react';
 
-export const CartContext = createContext()
+export const CartContext = createContext();
+
+const init = JSON.parse(localStorage.getItem('carrito')) || [];
 
 export const CartProvider = ({ children }) => {
 
-    const [carrito, setCarrito] = useState([])
+    const [carrito, setCarrito] = useState(init)
+
+    useEffect(() => {
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    }, [carrito])
+
 
     console.log(carrito);
 
@@ -37,7 +44,7 @@ export const CartProvider = ({ children }) => {
             removerItem,
             carrito,
             vaciarCarrito
-          }}>
+        }}>
             {children}
         </CartContext.Provider>
     )
